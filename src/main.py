c5f911f8488cfeed9581c5032deca9f8014d1451
snow7y <103980824+snow7y@users.noreply.github.com>
2025-05-10 22:19:11 +0900

    ruffの設定とビルドアクションの追加

--- src/main.py
+++ src/main.py
@@ -1,16 +1,17 @@
 import flet as ft
+from flet.core.types import OptionalControlEventCallable
 
 
-def main(page: ft.Page):
+def main(page: ft.Page) -> None:
     counter = ft.Text("0", size=50, data=0)
 
-    def increment_click(e):
+    def increment_click(_: OptionalControlEventCallable) -> None:
         counter.data += 1
         counter.value = str(counter.data)
         counter.update()
 
     page.floating_action_button = ft.FloatingActionButton(
-        icon=ft.Icons.ADD, on_click=increment_click
+        icon=ft.Icons.ADD, on_click=increment_click,
     )
     page.add(
         ft.SafeArea(
@@ -19,7 +20,7 @@ def main(page: ft.Page):
                 alignment=ft.alignment.center,
             ),
             expand=True,
-        )
+        ),
     )
 
 

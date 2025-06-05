aad3c3d75ad76056d99962ed8651f44214bc18d6
snow7y(yuki) <103980824+snow7y@users.noreply.github.com>
2025-06-05 13:58:26 +0000

    ビュー層のレイアウト管理を追加し、ルーティング処理を改善

--- src/router.py
+++ src/router.py
@@ -2,8 +2,11 @@
 # ページルーティングの管理を専用モジュールに分離
 import flet as ft
 
-from views.home import home_view
-from views.task import task_view
+from views import (
+    get_layout,
+    home_view,
+    task_view,
+)
 
 ROUTES = {
     "/": home_view,
@@ -21,18 +24,8 @@ def route_change(e: ft.RouteChangeEvent) -> None:
     page.views.clear()
     view_func = ROUTES.get(page.route)
     if view_func:
-        page.views.append(
-            ft.View(
-                page.route,
-                [view_func(page)],
-            ),
-        )
+        page.views.append(get_layout(page, view_func(page)))
     else:
         # 未定義ルートの場合はホームにリダイレクト
-        page.views.append(
-            ft.View(
-                "/",
-                [home_view(page)],
-            ),
-        )
+        page.views.append(get_layout(page, home_view(page)))
     page.update()
--- src/views/__init__.py
+++ src/views/__init__.py
@@ -1,2 +1,9 @@
-# view/__init__.py
-# ビュー層の初期化ファイル
+from home.view import home_view
+from layout import get_layout
+from task.view import task_view
+
+__all__ = [
+    "get_layout",
+    "home_view",
+    "task_view",
+]

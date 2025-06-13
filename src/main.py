9cc9d5db8bbdf1ecf49b50fc5ddf855ea28f7768
snow7y <103980824+snow7y@users.noreply.github.com>
2025-06-13 21:06:57 +0900

    .envファイルの作成と環境変数の管理機能を追加

--- src/main.py
+++ src/main.py
@@ -3,8 +3,12 @@ from loguru import logger
 from sqlmodel import SQLModel
 
 from config import APP_TITLE, engine
+from env import setup_environment
 from logging_conf import setup_logger
 from router import Router
+
+# 環境変数ファイルの作成
+setup_environment()
 
 # ログの設定
 setup_logger()

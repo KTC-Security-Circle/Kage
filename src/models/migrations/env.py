d268d0c832e369bc7d5e42ff72b10b01aa78ec5e
Kamome Kamone <kraken0711@gmail.com>
2025-08-04 12:49:12 +0900

    alembicの使い方の追記

--- src/models/migrations/env.py
+++ src/models/migrations/env.py
@@ -9,6 +9,7 @@ from alembic import context
 from sqlalchemy import engine_from_config, pool
 
 from src.config import Base
+from src.models.task import Task, TaskCreate
 
 # モデルをインポートしてAlembicがテーブル構造を認識できるようにする
 
@@ -26,8 +27,11 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata
-
+# target_metadata = Base.metadata
+target_metadata = [
+    Task.metadata,
+    TaskCreate.metadata,
+]
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")

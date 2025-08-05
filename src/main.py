6b1a392c48c9f64eb40608ac6fb5c362766ece41
snow7y <103980824+snow7y@users.noreply.github.com>
2025-08-06 00:44:53 +0900

    fix: 色の定義を修正
    main.pyのcolor_scheme_seedの値を小文字の'colors'から大文字の'Colors'に変更しました。

--- src/main.py
+++ src/main.py
@@ -28,7 +28,7 @@ def main(page: ft.Page) -> None:
     page.theme_mode = ft.ThemeMode.LIGHT
     page.padding = 0
     page.theme = ft.Theme(
-        color_scheme_seed=ft.colors.BLUE,
+        color_scheme_seed=ft.Colors.BLUE,
         page_transitions=ft.PageTransitionsTheme(
             windows=ft.PageTransitionTheme.NONE,
         ),

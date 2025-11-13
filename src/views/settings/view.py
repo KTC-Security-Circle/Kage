a4c44975467469d47924747a53703e84f52a657f
Kamome Kamone <kraken0711@gmail.com>
2025-11-13 16:43:04 +0900

    fix: 不要なコメントを削除し、コードの可読性を向上

--- src/views/settings/view.py
+++ src/views/settings/view.py
@@ -37,9 +37,8 @@ class SettingsView(BaseView):
 
         Args:
             props: ビュー共通プロパティ (page, apps などを含む)
-
+        """
         super().__init__(props)
-        page = props.page  # 既存セクション初期化との互換性用ローカルエイリアス
 
         # UI更新中フラグ（イベント発火を抑制）
         self._updating_ui = False
